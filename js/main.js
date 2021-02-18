75fd6c1b6bb0c961c1f8947845145f819ded8b72
DanielsKilLs20 <64350122+DanielsKilLs20@users.noreply.github.com>
2021-02-18 14:31:04 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,13 +2,15 @@ let num1 = 5;
 let num2 = 20;
 
 function wynik(num1 , num2){
-    if(num1 === num2){
-        return num1 === num2;
-    } else if (num1 <= num2){
-        return num1 <= num2;
-    } else{
-        return false;
-    }
-}
+    if (num1 === 20){
+        return num1 === 20;
 
-console.log(wynik( num1,num2));
+    } else if (num2 === 20){
+        return num2 === 20;
+
+    } else (num1 + num2 <= 20 )
+        return num1 + num2 <= 20;
+}
+   
+console.log(wynik(num1, num2));
+console.log(wynik(10,20));
